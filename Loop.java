35698b01b6ae9fcd89c3ddd0770d95c6c9f0eae0
lazkopat3522 <ikramamzkg@gmail.com>
2025-03-29 14:46:42 -0400

    Loop updte

--- Loop.java
+++ Loop.java
@@ -2,47 +2,51 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
-    public static void main(String []argh){
+    public static void main(String []argh) {
         Scanner in = new Scanner(System.in);
         int[] ar = new int[100000];
         int[] inar = new int[100000];
         int[] dat = new int[100000];
-        int c=0;
-        int c1=0;
-        int m=0;
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
+        int c = 0;
+        int c1 = 0;
+        int m = 0;
+        int t = in.nextInt();
+        for (int i = 0; i < t; i++) {
             int a = in.nextInt();
             int b = in.nextInt();
             int n = in.nextInt();
 
-            inar[c1]=n+m;
+            inar[c1] = n + m;
             c1++;
-            m=n;
-             for(int j=0;j<n;j++){
-                int power = (int) Math.pow(2,j);
+            m = n;
+            for (int j = 0; j < n; j++) {
+                int power = (int) Math.pow(2, j);
 
-                if(j==0) {
-                    ar[j]=a+power*b;
+                if (j == 0) {
+                    ar[j] = a + power * b;
                 } else {
-                    ar[j]=ar[j-1]+power*b;
+                    ar[j] = ar[j - 1] + power * b;
                 }
 
-                dat[c]=ar[j];
+                dat[c] = ar[j];
                 c++;
             }
 
         }
-        for (int j=0;j<inar.length;j++) {
 
-            for (int i = 0; i < c; i++)
-            if(i==inar[j]-1)
-                System.out.println(dat[i]+" ");
-            } else
-            {
-                System.out.print(dat[i] + " ");
+        int d=0;
+        for (int i = 0; i < c; i++) {
 
-            }
+            if (i == inar[d] - 1 && d<=inar.length) {
+                d++;
+                System.out.println(dat[i] + " ");
+
+
+        } else
+        {
+            System.out.print(dat[i] + " ");
+
+        }
 
         }
     }
