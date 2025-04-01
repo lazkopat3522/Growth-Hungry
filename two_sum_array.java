86356a94189dc248ffa7a25c55c1f00924db0f2c
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-01 11:43:27 -0400

    leetcode two sum

---
+++ two_sum_array.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class two_sum_array {
+    public static void main(String[] args) {
+        long int[] arr = new long int[10000000000];
+        Scanner sc=new Scanner(System.in);
+        int c=0;
+        System.out.print("nums = [");
+        while(System.in != "]"){
+
+         do
+             arr[c] = sc.nextInt();
+             c++;
+
+        }
+        System.out.print("target = ");
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]+" ");
+        }
+
+    }
+
+
+}
