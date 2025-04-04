fc8fa373e3f8d5bab9312bb5659655640a93ac51
lazkopat3522 <ikramamzkg@gmail.com>
2025-04-03 23:21:05 -0400

    digit

---
+++ Java/SBG.java
@@ -0,0 +1,11 @@
+package Java;
+
+import java.util.Scanner;
+
+public class SBG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n/10);
+    }
+}
